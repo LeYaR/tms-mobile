3a9bca15b7fe015e65690682358a71bbba3e3133
ArseniyVinokourov <77236075+ArseniyVinokourov@users.noreply.github.com>
2021-06-05 21:50:44 +0300

    Spring Security starter added

--- src/main/java/com/example/callaccountingsystem/CallAccountingSystemApplication.java
+++ src/main/java/com/example/callaccountingsystem/CallAccountingSystemApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CallAccountingSystemApplication {
+public class  CallAccountingSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CallAccountingSystemApplication.class, args);
