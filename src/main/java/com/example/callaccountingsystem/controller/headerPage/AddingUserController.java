9b2f20c1171ebaa7672db15139a9d5a4b6ca4c7d
Nikita Mazanik <mazanpro@mail.ru>
2021-06-09 15:26:26 +0300

    fixed double border & added Page adding/editing User

--- src/main/java/com/example/callaccountingsystem/controller/headerPage/AddingUserController.java
+++ src/main/java/com/example/callaccountingsystem/controller/headerPage/AddingUserController.java
@@ -1,17 +1,39 @@
 package com.example.callaccountingsystem.controller.headerPage;
 
+import com.example.callaccountingsystem.controller.Pagination;
+import com.example.callaccountingsystem.domain.dto.Address;
+import com.example.callaccountingsystem.domain.dto.Subscriber;
+import com.example.callaccountingsystem.service.AddressServiceInterface;
+import com.example.callaccountingsystem.service.SubscriberServiceInterface;
+import org.springframework.data.domain.Page;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/adding-user")
 public class AddingUserController {
+    private final SubscriberServiceInterface service;
+
+    public AddingUserController(SubscriberServiceInterface service) {
+        this.service = service;
+    }
 
     @GetMapping
-    public ModelAndView addingUserGet(Model model) {
+    public ModelAndView viewAllSubscribers(Model model,
+                                           @RequestParam("page") Optional<Integer> page,
+                                           @RequestParam("size") Optional<Integer> size) {
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(40);
+
+        final Page<Subscriber> subscriberPage = service.getAllSubscribers(currentPage, pageSize);
+        new Pagination().getPagination(model, currentPage, subscriberPage);
+        model.addAttribute("subscribers", subscriberPage);
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("addingUser");
--- src/main/java/com/example/callaccountingsystem/controller/headerPage/UserEditingController.java
+++ src/main/java/com/example/callaccountingsystem/controller/headerPage/UserEditingController.java
@@ -1,21 +1,22 @@
 package com.example.callaccountingsystem.controller.headerPage;
 
+import com.example.callaccountingsystem.service.SubscriberServiceInterface;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/user-editing")
 public class UserEditingController {
+    private final SubscriberServiceInterface service;
 
-    @GetMapping
-    public ModelAndView userEditingGet(Model model) {
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("userEditing");
-        return modelAndView;
+    public UserEditingController(SubscriberServiceInterface service) {
+        this.service = service;
     }
 
+    @GetMapping("/user-editing")
+    public ModelAndView userEditingGet(@RequestParam(name = "number") Long number, Model model) {
+        ModelAndView mav = new ModelAndView("userEditing");
+        mav.addObject("subscriber", service.get(number));
+        return mav;
+    }
 }
